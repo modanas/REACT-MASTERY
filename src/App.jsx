d04a5298119c3f1389f655abdcb93f238b27a212
m0zaid <mdzaid2798@gmail.com>
2025-07-19 22:59:11 +0530

    Dynamic title

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
 import './App.css'
 import About from './components/About'
+import Alert from './components/Alert';
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
 import React, { useState } from 'react';
 
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg : message,
+      type : type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000)
+  }
+
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
-      document.body.style.backgroundColor = "black";
+      document.body.style.backgroundColor = "#042743";
       document.body.style.color = "white";
+      showAlert("Dark mode has been enabled","success");
+      // document.title = "TextUtils - Dark Mode";
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       document.body.style.color = "black";
+      showAlert("Light mode has been enabled","success");
     }
   }
-  const [mode, setMode] = useState("light");
   return (
     <>
-      {/* <Navbar title="Anas" aboutText="Fucker"/> */}
+      {/* <Navbar title="Anas" aboutText="About"/> */}
       <Navbar mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm mode={mode} heading="Enter your text to analyze below"/>
+        <TextForm showAlert={showAlert} mode={mode} heading="Enter your text to analyze below"/>
         {/* <About /> */}
       </div>
     </>
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,44 +1,74 @@
 import React, { useState } from "react";
 
-
 export default function TextForm(props) {
- const handleUpperClick = () => {
-  let newText = text.toUpperCase();
-  setText(newText);
- }
- const handleLowerClick = () => {
-  let newLowText = text.toLowerCase();
-  setText(newLowText);
- }
- const handleOnChange = (event) => {
-  setText(event.target.value);
- }
- const handleReverseClick = () => {
-  let reversedText = '';
-  for (let i = text.length - 1; i >= 0; i--) {
-    reversedText += text[i];
-  }
-  console.log(reversedText);
-  setText(reversedText);
- }
- const [text, setText] = useState("");
+	const handleUpperClick = () => {
+		let newText = text.toUpperCase();
+		setText(newText);
+		props.showAlert("Converted to Uppercase","success");
+	};
+	const handleLowerClick = () => {
+		let newLowText = text.toLowerCase();
+		setText(newLowText);
+		props.showAlert("Converted to Lowercase","success");
+	};
+	const handleOnChange = (event) => {
+		setText(event.target.value);
+	};
+	const handleReverseClick = () => {
+		let reversedText = "";
+		for (let i = text.length - 1; i >= 0; i--) {
+			reversedText += text[i];
+			props.showAlert("Reversed text","success");
+		}
+		console.log(reversedText);
+		setText(reversedText);
+	};
+	const [text, setText] = useState("");
 
 	return (
-  <>
-				<div className="container mb-3">
-     <h1>{props.heading}</h1>
-      <textarea style={{backgroundColor : props.mode === 'dark' ? 'grey' : 'white', color : props.mode === 'dark' ? 'white' : 'black'}} value={text} onChange={handleOnChange} className="form-control" id="myBox" rows="8" cols="175">{text}</textarea>
-      <button onClick={handleUpperClick} className="btn btn-primary mt-2">Convert to Uppercase</button>
-      <button onClick={handleLowerClick} className="mx-4 btn btn-primary mt-2">Convert to Lowercase</button>
-      <button onClick={handleReverseClick} className="btn btn-primary mt-2">Reverse</button>
-				</div>
-    <div className="container my-3">
-     <h2>Your text summary</h2>
-     <p>{text.split(" ").length} words and {text.length}</p>
-     <p>{0.008 * text.split(" ").length} minutes read</p>
-     <h2>Preview</h2>
-     <p>{text.length>0 ? text : "Enter something in the textbox above to preview it here"}</p>
-    </div>
-    </>
+		<>
+			<div className="container mb-3" style={{ color: props.mode === "dark" ? "white" : "black" }}>
+				<h1>{props.heading}</h1>
+				<textarea
+					style={{
+						backgroundColor: props.mode === "dark" ? "grey" : "white",
+						color: props.mode === "dark" ? "white" : "black",
+					}}
+					value={text}
+					onChange={handleOnChange}
+					className="form-control"
+					id="myBox"
+					rows="8"
+					cols="175"
+				>
+					{text}
+				</textarea>
+				<button onClick={handleUpperClick} className="btn btn-primary mt-2">
+					Convert to Uppercase
+				</button>
+				<button
+					onClick={handleLowerClick}
+					className="mx-4 btn btn-primary mt-2"
+				>
+					Convert to Lowercase
+				</button>
+				<button onClick={handleReverseClick} className="btn btn-primary mt-2">
+					Reverse
+				</button>
+			</div>
+			<div className="container my-3" style={{ color: props.mode === "dark" ? "white" : "black" }}>
+				<h2>Your text summary</h2>
+				<p>
+					{text.split(" ").length} words and {text.length}
+				</p>
+				<p>{0.008 * text.split(" ").length} minutes read</p>
+				<h2>Preview</h2>
+				<p>
+					{text.length > 0
+						? text
+						: "Enter something in the textbox above to preview it here"}
+				</p>
+			</div>
+		</>
 	);
 }
